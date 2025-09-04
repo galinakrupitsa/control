020f818b6ae2146c0694f8170f137b0131bc413d
galina <galina.krupitsa@gmail.com>
2025-09-04 18:57:41 +0500

    kj

--- src/main/java/org/example/hello1/Controller11.java
+++ src/main/java/org/example/hello1/Controller11.java
@@ -1,20 +1,22 @@
 package org.example.hello1;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("api/publishers")
+@RequestMapping("/api")
 public class Controller11 {
-    private final Service11 service11;
-    public Controller11(Service11 service11, Service11 service111){
-    this.service11 = service111;
+//    private final Service11 service11;
+//    public Controller11(Service11 service11, Service11 service111){
+//    this.service11 = service111;
+
+
+@PostMapping("/publishers")
+public String result(@RequestBody UserRequest11 userRequest11) {
+    return "Писатель: " + userRequest11.getName() + ", Адрес:  " + userRequest11.getAddress();
+}
 }
 
-@GetMapping("api/publishers")
-public String printInfo(@RequestParam String name, @RequestParam String address) {
-        return service11.publisherInfo(name, address);
-}
-}
---
+++ src/main/java/org/example/hello1/UserRequest11.java
@@ -0,0 +1,13 @@
+package org.example.hello1;
+
+public class UserRequest11 {
+    private String name;
+    private String address;
+
+    // геттеры и сеттеры
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+}
